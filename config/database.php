d99fd1f20582fa38195cb2c456d6d8aa9ec370d3
Andres Garcia <andres.inginformatico@gmail.com>
2026-02-03 20:10:16 -0500

    x

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 $host = gethostbyname("sql305.infinityfree.com");
-$db   = "if0_40991675_santeria_db"; // 👈 NOMBRE REAL, NO XXX
+$db   = "if0_40991675_santeria"; // 👈 NOMBRE REAL, NO XXX
 $user = "if0_40991675";
 $pass = "4d84ka16na3g";
 
